8644890e88f7e5bd320af86c3869daba04fcef03
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-10-23 10:50:19 +0200

    Updated the content of the website

---
+++ src/script/scroll.js
@@ -0,0 +1,5 @@
+/*
+ * This file is responsible for the scrolling logic of the website.
+ * When the user scrolls, we will override the scrolling behaviour to fluidly
+ * scroll them to the next page.
+ */
