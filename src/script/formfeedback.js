2b3ecec1f8b19f10b4db90549ae64c17c9e785ea
le0271 <leonard.lns.123@gmail.com>
2018-10-31 23:46:10 +0100

    Implementing integer behaviour on number of seats field

--- src/script/formfeedback.js
+++ src/script/formfeedback.js
@@ -6,5 +6,36 @@
  */
 
 window.addEventListener("load", function() { // This block will run when site is finished loading
-    
+    let input_elements = document.querySelectorAll("#innerForm input");
+    // Name
+    let name_input_element = input_elements[0];
+    // Seats
+    let seats_input_element = input_elements[1];
+    seats_input_element.addEventListener("keydown", integer_field_keydown);
 });
+
+function integer_field_keydown(event) {
+    // Browser independant way to find the keyCode
+    let keyCode = event.which || event.keyCode || 0;
+    let pre_value = event.target.value;
+    console.log(keyCode);
+    // Allows backspace and tab
+    if (keyCode == 8 || keyCode == 9) {
+        return;
+    }
+    // Checks for existing number in field
+    if (pre_value.length == 0) { // If none:
+        if (49 <= keyCode && keyCode <= 57) { // Checks if input is a number between 1 and 9
+            return; // Allows such number
+        }
+    } else if (pre_value.length == 1 || pre_value.length == 2) { // If one or two digit number:
+        if (48 <= keyCode && keyCode <= 57) { // Checks if input is a number between 0 and 9
+            return; // Allows such number
+        }
+    }
+    event.preventDefault(); // Inhibits all other buttonpresses
+}
+
+function submit_to_server(name, seats, phone, date) {
+    
+}
