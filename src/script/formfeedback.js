1748356a47ce50ae2c16e75b70510c9b7e798379
le0271 <leonard.lns.123@gmail.com>
2018-11-01 00:29:45 +0100

    Implementing number field

--- src/script/formfeedback.js
+++ src/script/formfeedback.js
@@ -5,7 +5,7 @@
  * Written by Leonard
  */
 
-const STANDARD_DIGITS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const DIGIT_CHARACTERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 window.addEventListener("load", function() { // This block will run when site is finished loading
     let input_elements = document.querySelectorAll("#innerForm input");
@@ -14,14 +14,17 @@ window.addEventListener("load", function() { // This block will run when site is
     // Seats
     let seats_input_element = input_elements[1];
     seats_input_element.addEventListener("input", integer_field_input);
+    // Phone number
+    let phone_input_element = input_elements[2];
+    phone_input_element.addEventListener("input", phone_field_input);
 });
 
 function integer_field_input(event) {
-    let pre_value = event.target.value;
-    let new_value = "";
+    let pre_value = event.target.value; // Non-modified value
+    let new_value = ""; // Current modified value
     for (let i = 0; i < pre_value.length; i++) {
         if (
-            STANDARD_DIGITS.includes(pre_value[i]) || // Is character digit from 1 to 9
+            DIGIT_CHARACTERS.includes(pre_value[i]) && pre_value[i] != "0" || // Is character digit from 1 to 9
             pre_value[i] == "0" && new_value.length != 0 // Is character 0 and is it preceded by other numbers
         ) {
             // Add digit to number
@@ -32,6 +35,38 @@ function integer_field_input(event) {
         }
     }
     event.target.value = new_value; // Changes field value to modified value
+}
+
+function phone_field_input(event) {
+    let pre_value = event.target.value; // Non-modified value
+    let new_value = ""; // Current modified value
+    for (let i = 0; i < pre_value.length; i++) {
+        if (DIGIT_CHARACTERS.includes(pre_value[i])) {
+            new_value += pre_value[i];
+            if (new_value.length == 8) {
+                break;
+            }
+        }
+    }
+    if (new_value[0] == "4" || new_value[0] == "8" || new_value[0] == "9") {
+        if (new_value.length > 5) {
+            new_value = [new_value.slice(0, 5), " ", new_value.slice(5)].join("");
+        }
+        if (new_value.length > 3) {
+            new_value = [new_value.slice(0, 3), " ", new_value.slice(3)].join("");
+        }
+    } else {
+        if (new_value.length > 6) {
+            new_value = [new_value.slice(0, 6), " ", new_value.slice(6)].join("");
+        }
+        if (new_value.length > 4) {
+            new_value = [new_value.slice(0, 4), " ", new_value.slice(4)].join("");
+        }
+        if (new_value.length > 2) {
+            new_value = [new_value.slice(0, 2), " ", new_value.slice(2)].join("");
+        }
+    }
+    event.target.value = new_value;
 }
 
 function submit_to_server(name, seats, phone, date) {
