2d9f4d546fc444f992d71e32ae9d8fef6e0ece47
le0271 <leonard.lns.123@gmail.com>
2018-10-31 23:03:53 +0100

    Creating and linking to form feedback script

---
+++ src/script/formfeedback.js
@@ -0,0 +1,10 @@
+/**
+ * This script will modify the input elements in the form to ensure the values
+ * in them is always the correct format and do necessary actions when the user
+ * submits the table reservation form.
+ * Written by Leonard
+ */
+
+window.addEventListener("load", function() { // This block will run when site is finished loading
+    
+});
