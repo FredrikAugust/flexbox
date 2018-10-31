b5a1466be3806d55fae2f93ba6a753094a74972e
Erik <eriks.skar@online.ntnu.no>
2018-10-31 02:26:10 +0100

    added items to the menus (WIP) and some extra styling for the menus including a seasonal border-color

--- src/script/scroll.js
+++ src/script/scroll.js
@@ -2,6 +2,7 @@
  * This file is responsible for the scrolling logic of the website.
  * When the user scrolls, we will override the scrolling behaviour to fluidly
  * scroll them to the next page.
+ * Written completely by Fredrik August Madsen-Malmo
  */
 
 let previousScrollPosition = 0;
